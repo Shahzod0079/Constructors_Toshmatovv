8b23a5bc18c6504ceac8acb6a3f1d800424e1200
student-a502 <student-a502@STUD-RM11-A502>
2025-11-07 18:06:00 +0500

    Разработанный класс Student. Тошматов

---
+++ Classes/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Toshmatovv.Classes
+{
+
+    public class Student
+    {
+        public string Firstname = "";
+
+        public string Lastname = "";
+
+        public string Surname = "";
+
+        public bool Scholarship = false;
+
+        public int Course = 4;
+
+        public Student(string firstname, string lastname, string surname)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Surname = surname;
+        }
+
+        public Student(string firstname, string lastname, string surname, bool scholarship)
+            : this(firstname, lastname, surname)
+        {
+            this.Scholarship = scholarship;
+        }
+
+        public Student(string firstname, string lastname, string surname, bool scholarship, int course)
+            : this(firstname, lastname, surname, scholarship)
+        {
+            this.Course = course;
+        }
+
+        public string GetFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
